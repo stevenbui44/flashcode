cc4fd3c76412ddd19baec7c81a593876e90ace1b
shbui <shbui@ncsu.edu>
2024-05-26 10:52:49 -0400

    Initial commit

---
+++ flashcode/src/main/java/com/stevenbui/flashcode/FlashcodeApplication.java
@@ -0,0 +1,13 @@
+package com.stevenbui.flashcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcodeApplication.class, args);
+	}
+
+}
---
+++ flashcode/src/test/java/com/stevenbui/flashcode/FlashcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevenbui.flashcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
