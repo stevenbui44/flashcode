40fd44797a8d45cbd4493cd2a2b01e9e9573438b
stevenbui44 <stevenbui44@gmail.com>
2024-06-01 17:47:48 -0400

    [API] - Get cards in an assortment API
    Created the REST API GET call for getting the cards for a particular
    assortment's id. Still need to create the HTML file.

--- flashcode/src/main/java/com/stevenbui/flashcode/controllers/MappingController.java
+++ flashcode/src/main/java/com/stevenbui/flashcode/controllers/MappingController.java
@@ -1,11 +1,19 @@
 package com.stevenbui.flashcode.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.stevenbui.flashcode.models.Assortment;
+import com.stevenbui.flashcode.repositories.AssortmentRepository;
 
 @Controller
 public class MappingController {
+
+    @Autowired
+    private AssortmentRepository assortmentRepository;
 
     /**
      * Method invoked whenever a GET request is made to either /cards or
@@ -20,7 +28,7 @@ public class MappingController {
      * @return contents of src/main/resources/templates/file-cards.html
      */
     @GetMapping ( { "/cards", "/cards.html" } )
-    public String getAllCards ( final Model model ) {
+    public String getAssortmentCards ( final Model model ) {
         return "file-cards";
     }
 
@@ -36,9 +44,17 @@ public class MappingController {
      *            the underlying UI model
      * @return contents of src/main/resources/templates/file-assortments.html
      */
-    @GetMapping ( { "/assortments", "assortments.html" } )
+    @GetMapping ( { "/assortments", "assortments.html", "/" } )
     public String getAllAssortments ( final Model model ) {
         return "file-assortments";
+    }
+
+    @GetMapping ( "/assortment/{id}/cards" )
+    public String getAssortmentCards ( @PathVariable ( "id" ) final Long assortmentId, final Model model ) {
+        final Assortment assortment = assortmentRepository.findById( assortmentId )
+                .orElseThrow( () -> new IllegalArgumentException( "Invalid assortment ID: " + assortmentId ) );
+        model.addAttribute( "assortment", assortment );
+        return "file-assortment-cards";
     }
 
 }
