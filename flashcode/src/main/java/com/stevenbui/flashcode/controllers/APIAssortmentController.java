ae1a61401d04f6545cf11d366851b1bd94501740
stevenbui44 <stevenbui44@gmail.com>
2024-06-02 12:54:32 -0400

    [API] - api/v1/assortments/{id}
    Created an API endpoint in APIAssortmentController that gets the data
    for a given assortment's cards

--- flashcode/src/main/java/com/stevenbui/flashcode/controllers/APIAssortmentController.java
+++ flashcode/src/main/java/com/stevenbui/flashcode/controllers/APIAssortmentController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stevenbui.flashcode.models.Assortment;
+import com.stevenbui.flashcode.models.Card;
 import com.stevenbui.flashcode.services.AssortmentService;
+
+import jakarta.persistence.EntityNotFoundException;
 
 /**
  * Handles REST API endpoints for flash card sets
@@ -36,6 +40,15 @@ public class APIAssortmentController extends APIController {
     @GetMapping ( BASE_PATH + "/assortments" )
     public List<Assortment> getAssortments () {
         return assortmentService.findAll();
+    }
+
+    @GetMapping ( BASE_PATH + "/assortments/{id}" )
+    public List<Card> getAssortmentCards ( @PathVariable ( "id" ) final Long assortmentId ) {
+        final Assortment assortment = assortmentService.findById( assortmentId );
+        if ( assortment == null ) {
+            throw new EntityNotFoundException( "Assortment not found with ID: " + assortmentId );
+        }
+        return assortment.getCards();
     }
 
 }
