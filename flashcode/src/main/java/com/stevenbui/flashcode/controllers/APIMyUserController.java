c4d34fb423abf5c189f65708757604a108664a68
stevenbui44 <stevenbui44@gmail.com>
2024-06-19 11:40:38 -0400

    [Security][Implementation] - Delete account
    Added functionality for deleting an account from the database, and it
    sends you to the /login page. A modal pops up to confirm too. Logging in
    with the account will not work. I want to see if I can make it so that
    instead of a button, you can press your username and have a dropdown
    come up instead.
    
    - #18

--- flashcode/src/main/java/com/stevenbui/flashcode/controllers/APIMyUserController.java
+++ flashcode/src/main/java/com/stevenbui/flashcode/controllers/APIMyUserController.java
@@ -1,14 +1,49 @@
 package com.stevenbui.flashcode.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stevenbui.flashcode.models.MyUser;
 import com.stevenbui.flashcode.repositories.MyUserRepository;
+import com.stevenbui.flashcode.services.MyUserService;
 
 @RestController
 public class APIMyUserController extends APIController {
 
     @Autowired
     private MyUserRepository myUserRepository;
+
+    @Autowired
+    private MyUserService    myUserService;
+
+    @GetMapping ( "/api/v1/users/current" )
+    public ResponseEntity< ? > getCurrentUser () {
+        final MyUser currentUser = myUserService.getCurrentUser();
+        if ( currentUser != null ) {
+            return ResponseEntity.ok( currentUser );
+        }
+        else {
+            return ResponseEntity.badRequest().body( "No user is currently logged in." );
+        }
+    }
+
+    @DeleteMapping ( "/api/v1/users/{userId}" )
+    public ResponseEntity< ? > deleteUser ( @PathVariable final Long userId ) {
+        final Optional<MyUser> optionalUser = myUserRepository.findById( userId );
+        if ( optionalUser.isPresent() ) {
+            final MyUser user = optionalUser.get();
+            myUserRepository.delete( user );
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.badRequest().body( "User not found." );
+        }
+    }
 
 }
