ca6b0826b119e9fcf40cce3acd37c3d657d7b3ae
stevenbui44 <stevenbui44@gmail.com>
2024-06-23 16:44:21 -0400

    [CSS] - Dark mode is default
    Changed dark mode to be the default theme. I'm also only going to be
    using dark and light themes.
    
    - #25
    - #28

--- flashcode/src/main/resources/static/js/theme.js
+++ flashcode/src/main/resources/static/js/theme.js
@@ -1,58 +1,5 @@
 const themes = {
-    //default: {
-    //    '--background-color': '#f9f7f1',		// tan			(background)
-    //    '--header-color': '#1a2a57',			// dark blue 	(header)
-    //    '--assortment-background': '#ffffff',	// white 		(assortment main box)
-    //    '--assortment-border': '#ddd',			// light grey 	(border)
-    //    '--text-primary': '#333333',			// dark grey 	(main text)
-    //    '--text-secondary': '#666666',			// medium grey 	(description)
-    //    '--card-count-background': '#f0f0f0',	// light grey	(assortment cards box)
-    //    '--title-color': '#000000',
-    //    '--no-description-color': '#999999',
-    //    '--search-text-color': '#333333'
-    //},
-    
-    default: {
-        '--header-color': '#4F5459',
-        '--background-color': '#E1E1E1',
-        
-        '--assortment-background': '#EFEFEF',
-        '--card-count-background': '#B8BEC4',
-        '--assortment-border': '#E1E1E1',
-        
-        '--title-color': '#443E46',
-        '--text-primary': '#443E46',
-        '--text-secondary': '#443E46',
-        '--no-description-color': '#443E46',
-        '--search-text-color': '#443E46',
-        '--no-assortments-text': '#333333',
-        
-        '--search-input-background': '#EFEFEF',
-        '--search-input-text': '#333333',
-        '--search-input-placeholder': '#999999',
-        
-        '--button-text': '#EFEFEF',
-        
-        '--username-text': '#ffffff',
-        '--profile-pic-background': '#808080',
-        '--profile-pic-text': '#ffffff'
-    },
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    dark: {
+	default: {
         '--background-color': '#232124',
         '--header-color': '#302D32',
         
@@ -79,37 +26,30 @@ const themes = {
         '--profile-pic-text': '#ffffff',
         '--flashcode-title-color': '#ffffff'
     },
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     light: {
-        '--background-color': '#ffffff',
-        '--header-color': '#111951',
-        '--assortment-background': '#f9f9f9',
-        '--assortment-border': '#e0e0e0',
-        '--text-primary': '#333333',
-        '--text-secondary': '#666666',
-        '--card-count-background': '#f0f0f0',
-        '--title-color': '#000000',
-        '--no-description-color': '#999999',
-        '--search-text-color': '#ffffff',
+        '--header-color': '#4F5459',
+        '--background-color': '#E1E1E1',
+        
+        '--assortment-background': '#EFEFEF',
+        '--card-count-background': '#B8BEC4',
+        '--assortment-border': '#E1E1E1',
+        
+        '--title-color': '#443E46',
+        '--text-primary': '#443E46',
+        '--text-secondary': '#443E46',
+        '--no-description-color': '#443E46',
+        '--search-text-color': '#443E46',
+        '--no-assortments-text': '#333333',
+        
+        '--search-input-background': '#EFEFEF',
+        '--search-input-text': '#333333',
+        '--search-input-placeholder': '#999999',
+        
+        '--button-text': '#EFEFEF',
+        
+        '--username-text': '#ffffff',
+        '--profile-pic-background': '#808080',
+        '--profile-pic-text': '#ffffff'
     }
 };
 
